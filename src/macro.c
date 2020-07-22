83db40db05dc8dc21c9930d8dc7881a2b14805c7
Elio-yang <2925539854@qq.com>
2020-07-23 01:03:11 +0800

    format and comments

--- src/macro.c
+++ src/macro.c
@@ -9,13 +9,13 @@
  * @param type 类型（struct）
  * @param member 成员
  */
+/* (type *)0                         转化为0型指针，即地址为0
+ * ((type *)0)->member               获取成员
+ * &(((type *)0)->member)            获取成员地址
+ * (size_t)&(((type *)0)->member)    将地址转化为size_t 即为偏移量
+ */
 #define __offsetof__(type, member) ( ( size_t ) & ( ( type * ) 0 )->member )
-/*
-* (type *)0                         转化为0型指针，即地址为0
-* ((type *)0)->member               获取成员
-* &(((type *)0)->member)            获取成员地址
-* (size_t)&(((type *)0)->member)    将地址转化为size_t 即为偏移量
-*/
+
 
 /**
  * 获取指向整个结构体的指针
@@ -23,18 +23,17 @@
  * @param type 类型(struct)
  * @param member 成员变量
  */
+/* typeof()                                                   为GNU C Extension 获得变量类型
+ * typeof (((type *)0 )->member)                              先是转化为0型指针，起始地址为0 再获取member 最后返回member类型
+ * const typeof (((type *)0 )->member) *__mptr=(ptr)          定义__mptr 指针，指向ptr指向的地址，并成为常量指针
+ * (char *)__mptr                                             __mptr转化为字符型指针(运算以1个字节为单位)
+ * - __offsetof__(type,member))                               减去该成员的偏移量
+ * (type*)( ( char * )__mptr - __offsetof__(type,member))     最后转化为指向该类型的指针(指向该类型的首地址)
+ */
 #define __container_of__(ptr, type, member) ({\
                 const typeof ( ( ( type * ) 0 ) -> member ) *__mptr=(ptr);\
                 ( type * )( ( char * )__mptr - __offsetof__( type, member ) );\
                 })
-/*
- * typeof()                                                         为GNU C Extension 获得变量类型
- * typeof (((type *)0 )->member)                                    先是转化为0型指针，起始地址为0 再获取member 最后返回member类型
- * const typeof (((type *)0 )->member) *__mptr=(ptr)                定义__mptr 指针，指向ptr指向的地址，并成为常量指针
- * (char *)__mptr                                                   __mptr转化为字符型指针(运算以1个字节为单位)
- * - __offsetof__(type,member))                                     减去该成员的偏移量
- * (type*)( ( char * )__mptr - __offsetof__(type,member))           最后转化为指向该类型的指针(指向该类型的首地址)
- */
 
 typedef struct Student {
     char gender;
