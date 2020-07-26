d073a438306c7fe260f0dbba2826b6950e9a0a33
Elio-yang <2925539854@qq.com>
2020-07-26 21:53:49 +0800

    gitignore

--- src/link.c
+++ src/link.c
@@ -15,7 +15,7 @@ struct student{
 
 int main()
 {
-        struct student *astudent;
+        struct student *a_student;
         struct student student_head;
         struct list_head *pos, *next;
         int i;
@@ -23,32 +23,32 @@ int main()
         INIT_LIST_HEAD(&student_head.list);
 
         for (int j = 0; j < 5; j++) {
-                astudent=(struct student *)malloc(sizeof(struct student));
-                astudent->age= 10 * j + 1;
-                sprintf(astudent->name, "%d", j + 1);
-                list_add_tail(&(astudent->list),&(student_head.list));
+                a_student=(struct student *)malloc(sizeof(struct student));
+                a_student->age= 10 * j + 1;
+                sprintf(a_student->name, "%d", j + 1);
+                list_add_tail(&(a_student->list), &(student_head.list));
         }
         printf("========== 1st iterator link =========\n");
         list_for_each(pos, &student_head.list) {
-                astudent = list_entry(pos, struct student, list);
-                printf("student age: %2d student name: %s\n", astudent->age, astudent->name);
+                a_student = list_entry(pos, struct student, list);
+                printf("student age: %2d student name: %s\n", a_student->age, a_student->name);
         }
         list_for_each_safe(pos,next,&student_head.list){
-                astudent=list_entry(pos,struct student,list);
-                if (astudent->age==21){
+                a_student=list_entry(pos, struct student, list);
+                if (a_student->age == 21){
                         list_del_init(pos);
-                        free(astudent);
+                        free(a_student);
                 }
         }
         printf("========== 2nd iterator link =========\n");
         list_for_each(pos, &student_head.list) {
-                astudent = list_entry(pos, struct student, list);
-                printf("student age: %2d student name: %s\n", astudent->age, astudent->name);
+                a_student = list_entry(pos, struct student, list);
+                printf("student age: %2d student name: %s\n", a_student->age, a_student->name);
         }
         list_for_each_safe(pos,next,&student_head.list){
-                astudent=list_entry(pos,struct student,list);
+                a_student=list_entry(pos, struct student, list);
                 list_del_init(pos);
-                free(astudent);
+                free(a_student);
         }
 
         return 0;
